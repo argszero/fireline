37f1e129b0c523b03aa1e605d97e4b4bc02ceabd
argszero <argszero@gmail.com>
2012-03-31 11:13:59 +0800

    remove useless comments

--- fireline/src/fireline.js
+++ fireline/src/fireline.js
@@ -9,16 +9,3 @@ web.listen(program.port);
 signin.signin();
 signin.listen();
 
-
-//var http = require('http');
-//var signin = require('./signin');
-//
-//
-//http.createServer(function (request, response) {
-//	response.writeHead(200, {'Content-Type': 'text/plain'});
-//	response.end('Hello World\n');
-//}).listen(8124);
-//
-
-//console.log('Server running at http://127.0.0.1:8124/');
-
